31a3b24b569331d4b9562356ca8adc26965c1aaf
rolferas <rolfneto@gmail.comgit statusgit statusstatus>
2019-12-10 22:33:36 -0300

    adding projects

---
+++ Jamming (react)/build/precache-manifest.46d89256ef9a5efdfd06951b2f90ef15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc027834533357dc3f48c9749a9a7ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "114477740fea04d89a45",
+    "url": "/static/css/main.f64744eb.chunk.css"
+  },
+  {
+    "revision": "d70fe0970e9e90d2979a",
+    "url": "/static/js/2.38ef569c.chunk.js"
+  },
+  {
+    "revision": "114477740fea04d89a45",
+    "url": "/static/js/main.05ea7672.chunk.js"
+  },
+  {
+    "revision": "57f40e676f2f22797492",
+    "url": "/static/js/runtime~main.2cf770a5.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
---
+++ Jamming (react)/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onClick={this.search} />
+                <button className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
